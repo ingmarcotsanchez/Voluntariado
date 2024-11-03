b7e1210bb8a798627b4aaea1e9913ad15bd3f1af
ingmarcotsanchez <ing.marcotsanchez@gmail.com>
2024-11-02 21:38:25 -0500

    lugares

---
+++ controller/lugares.php
@@ -0,0 +1,105 @@
+<?php
+    /*TODO: Llamando a cadena de Conexion */
+    require_once("../config/conexion.php");
+    /*TODO: Llamando a la clase */
+    require_once("../models/Lugares.php");
+    /*TODO: Inicializando Clase */
+    $lugares = new Lugares();
+
+    /*TODO: Opcion de solicitud de controller */
+    switch($_GET["opc"]){
+        /*TODO: Guardar y editar cuando se tenga el ID */
+        case "guardaryeditar":
+            if(empty($_POST["lug_id"])){
+                $lugares->insert_lugares($_POST["area_id"],$_POST["lug_nom"],$_POST["lug_descrip"],$_POST["lug_fechini"],$_POST["lug_fechfin"],$_POST["ext_id"]);
+            }else{
+                $lugares->update_lugares($_POST["lug_id"],$_POST["area_id"],$_POST["lug_nom"],$_POST["lug_descrip"],$_POST["lug_fechini"],$_POST["lug_fechfin"],$_POST["ext_id"]);
+            }
+            break;
+        /*TODO: Creando Json segun el ID */
+        case "mostrar":
+            $datos = $lugares->get_lugares_id($_POST["lug_id"]);
+            if(is_array($datos)==true and count($datos)<>0){
+                foreach($datos as $row){
+                    $output["lug_id"] = $row["lug_id"];
+                    $output["area_id"] = $row["area_id"];
+                    $output["lug_nom"] = $row["lug_nom"];
+                    $output["lug_descrip"] = $row["lug_descrip"];
+                    $output["lug_fechini"] = $row["lug_fechini"];
+                    $output["lug_fechfin"] = $row["lug_fechfin"];
+                    $output["ext_id"] = $row["ext_id"];
+                }
+                echo json_encode($output);
+            }
+            break;
+        /*TODO: Eliminar segun ID */
+        case "eliminar":
+            $lugares->delete_lugares($_POST["lug_id"]);
+            break;
+        /*TODO:  Listar toda la informacion segun formato de datatable */
+        case "listar":
+            $datos=$lugares->get_lugares();
+            $data= Array();
+            foreach($datos as $row){
+                $sub_array = array();
+                $sub_array[] = $row["area_nom"];
+                $sub_array[] = '<a href="'.$row["lug_img"].'" target="_blank">'.strtoupper($row["lug_nom"]).'</a>';
+                $sub_array[] = $row["lug_fechini"];
+                $sub_array[] = $row["lug_fechfin"];
+                $sub_array[] = $row["ext_nom"] ." ". $row["ext_ape"];
+                $sub_array[] = '<button type="button" onClick="editar('.$row["lug_id"].');"  id="'.$row["lug_id"].'" class="btn btn-outline-warning btn-icon btn-sm"><div><i class="fa fa-edit"></i></div></button>';
+                $sub_array[] = '<button type="button" onClick="eliminar('.$row["lug_id"].');"  id="'.$row["lug_id"].'" class="btn btn-outline-danger btn-icon btn-sm"><div><i class="fa fa-close"></i></div></button>';                
+                $sub_array[] = '<button type="button" onClick="imagen('.$row["lug_id"].');"  id="'.$row["lug_id"].'" class="btn btn-outline-success btn-icon btn-sm"><div><i class="fa fa-file"></i></div></button>';                
+                $data[] = $sub_array;
+            }
+
+            $results = array(
+                "sEcho"=>1,
+                "iTotalRecords"=>count($data),
+                "iTotalDisplayRecords"=>count($data),
+                "aaData"=>$data);
+            echo json_encode($results);
+            break;
+        /*TODO:  Listar toda la informacion segun formato de datatable */
+        case "combo":
+            $datos=$lugares->get_lugares();
+            if(is_array($datos)==true and count($datos)>0){
+                $html= " <option label='Seleccione'></option>";
+                foreach($datos as $row){
+                    $html.= "<option value='".$row['lug_id']."'>".$row['lug_nom']."</option>";
+                }
+                echo $html;
+            }
+            break;
+
+        case "eliminar_lugares_usuario":
+            $lugares->delete_lugares_usuario($_POST["lugd_id"]);
+            break;
+        /*TODO: Insetar detalle de curso usuario */
+        case "insert_lugares_usuario":
+            /*TODO: Array de usuario separado por comas */
+            $datos = explode(',', $_POST['usu_id']);
+            /*TODO: Registrar tantos usuarios vengan de la vista */
+            $data = Array();
+            foreach($datos as $row){
+                $sub_array = array();
+                $idx=$curso->insert_lugares_usuario($_POST["lug_id"],$row);
+                $sub_array[] = $idx;
+                $data[] = $sub_array;
+            }
+
+            echo json_encode($data);
+            break;
+
+        case "generar_qr":
+            require 'phpqrcode/qrlib.php';
+            //Primer Parametro - Text del QR
+            //Segundo Parametro - Ruta donde se guardara el archivo
+            QRcode::png(conectar::ruta()."view/certificado.php?lugd_id=".$_POST["lugd_id"],"../public/qr/".$_POST["lugd_id"].".png",'L',32,5);
+            break;
+
+        case "update_imagen_lugares":
+            $lugares->update_imagen_lugares($_POST["lugx_idx"],$_POST["lug_img"]);
+            break;
+    }
+?>
---
+++ models/Lugares.php
@@ -0,0 +1,157 @@
+<?php
+    class Lugares extends Conectar{
+
+        public function insert_lugares($area_id,$lug_nom,$lug_descrip,$lug_fechini,$lug_fechfin,$ext_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO lugares (lug_id, area_id, lug_nom, lug_descrip, lug_fechini, lug_fechfin, ext_id,lug_img, fech_crea, est) VALUES (NULL,?,?,?,?,?,?,'../../public/1.png', now(),'1');";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $area_id);
+            $sql->bindValue(2, $lug_nom);
+            $sql->bindValue(3, $lug_descrip);
+            $sql->bindValue(5, $lug_fechfin);
+            $sql->bindValue(4, $lug_fechini);
+            $sql->bindValue(6, $ext_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function update_lugares($lug_id,$area_id,$lug_nom,$lug_descrip,$lug_fechini,$lug_fechfin,$ext_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE lugares
+                SET
+                    area_id =?,
+                    lug_nom = ?,
+                    lug_descrip = ?,
+                    lug_fechini = ?,
+                    lug_fechfin = ?,
+                    ext_id = ?
+                WHERE
+                    lug_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $area_id);
+            $sql->bindValue(2, $lug_nom);
+            $sql->bindValue(3, $lug_descrip);
+            $sql->bindValue(5, $lug_fechfin);
+            $sql->bindValue(4, $lug_fechini);
+            $sql->bindValue(6, $ext_id);
+            $sql->bindValue(7, $lug_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function delete_lugares($lug_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE lugares
+                SET
+                    est = 0
+                WHERE
+                    lug_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $lug_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function get_lugares(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT
+                lugares.lug_id,
+                lugares.lug_nom,
+                lugares.lug_descrip,
+                lugares.lug_fechini,
+                lugares.lug_fechfin,
+                lugares.area_id,
+                lugares.lug_img,
+                areas.area_nom,
+                lugares.ext_id,
+                externos.ext_nom,
+                externos.ext_ape,
+                externos.ext_correo,
+                externos.ext_telf
+                FROM lugares
+                INNER JOIN areas on lugares.area_id = areas.area_id
+                INNER JOIN externos on lugares.ext_id = externos.ext_id
+                WHERE lugares.est = 1";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function get_lugares_id($lug_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM lugares WHERE est = 1 AND lug_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $lug_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function delete_lugares_usuario($lugd_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE lugares_usuario
+                SET
+                    est = 0
+                WHERE
+                    lugd_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $lugd_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        /*TODO: Insert Curso por Usuario */
+        public function insert_lugares_usuario($lug_id,$usu_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO lugares_usuario (lugd_id,lug_id,usu_id,fech_crea,est) VALUES (NULL,?,?,now(),1);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $lug_id);
+            $sql->bindValue(2, $usu_id);
+            $sql->execute();
+
+            $sql1="select last_insert_id() as 'lugd_id'";
+            $sql1=$conectar->prepare($sql1);
+            $sql1->execute();
+            return $resultado=$sql1->fetch(pdo::FETCH_ASSOC);
+        }
+
+        public function update_imagen_lugares($lug_id,$lug_img){
+            $conectar= parent::conexion();
+            parent::set_names();
+
+            require_once("Lugares.php");
+            $curx = new Lugares();
+            $lug_img = '';
+            if ($_FILES["lug_img"]["name"]!=''){
+                $lug_img = $curx->upload_file();
+            }
+
+            $sql="UPDATE lugares
+                SET
+                    lug_img = ?
+                WHERE
+                    lug_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $lug_img);
+            $sql->bindValue(2, $lug_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function upload_file(){
+            if(isset($_FILES["lug_img"])){
+                $extension = explode('.', $_FILES['lug_img']['name']);
+                $new_name = rand() . '.' . $extension[1];
+                $destination = '../public/' . $new_name;
+                move_uploaded_file($_FILES['lug_img']['tmp_name'], $destination);
+                return "../../public/".$new_name;
+            }
+        }
+    }
+?>
