051a34b868433e22ba7c8a8ddf94167d291840a8
ingmarcotsanchez <ing.marcotsanchez@gmail.com>
2024-11-16 12:42:37 -0500

    pruebas tickets

--- controller/ticket.php
+++ controller/ticket.php
@@ -2,7 +2,7 @@
     require_once("../config/conexion.php");
     require_once("../models/Ticket.php");
     $ticket = new Ticket();
-    require_once("../models/Usuario.php");
+    require_once("../models/Usuarios.php");
     $usuario = new Usuario();
     require_once("../models/Documento.php");
     $documento = new Documento();
